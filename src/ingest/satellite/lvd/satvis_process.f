f203caac5dbcce3cfa406b2ed960fe9edf45e5dd
Steve Albers <stevealbers@Steve-Albers-MacBook-Pro.local>
2018-11-06 20:16:27 +0000

    improved logging and debugging

--- src/ingest/satellite/lvd/satvis_process.f
+++ src/ingest/satellite/lvd/satvis_process.f
@@ -1,34 +1,3 @@
-cdis    Forecast Systems Laboratory
-cdis    NOAA/OAR/ERL/FSL
-cdis    325 Broadway
-cdis    Boulder, CO     80303
-cdis 
-cdis    Forecast Research Division
-cdis    Local Analysis and Prediction Branch
-cdis    LAPS 
-cdis 
-cdis    This software and its documentation are in the public domain and 
-cdis    are furnished "as is."  The United States government, its 
-cdis    instrumentalities, officers, employees, and agents make no 
-cdis    warranty, express or implied, as to the usefulness of the software 
-cdis    and documentation for any purpose.  They assume no responsibility 
-cdis    (1) for the use of the software and documentation; or (2) to provide
-cdis     technical support to users.
-cdis    
-cdis    Permission to use, copy, modify, and distribute this software is
-cdis    hereby granted, provided that the entire disclaimer notice appears
-cdis    in all copies.  All modifications to this software must be clearly
-cdis    documented, and are solely the responsibility of the agent making 
-cdis    the modifications.  If significant modifications or enhancements 
-cdis    are made to this software, the FSL Software Policy Manager  
-cdis    (softwaremgr@fsl.noaa.gov) should be notified.
-cdis 
-cdis 
-cdis 
-cdis 
-cdis 
-cdis 
-cdis 
       subroutine process_vis_satellite(csatid,
      &           c_sat_type,
      &           i4time,                                           ! I
@@ -182,8 +151,8 @@ c
 c Call the visible satellite data remapping subroutine.
 c
        write(6,*)
-       write(6,*)' Processing visible satellite data.'
-       write(6,*)' ----------------------------------'
+       write(6,*)' Processing visible satellite data - ',c_sat_type
+       write(6,*)' ---------------------------------------'
 
        call array_range(image_vis,n_vis_elem,n_vis_lines,rmin,rmax
      1                 ,r_missing_data)
